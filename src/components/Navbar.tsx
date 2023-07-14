5af37417734cb6cdd87da903c4ecc18ba4b5239d
4nth0ny1 <acatullo4@gmail.com>
2023-07-13 23:06:42 -0500

    added daisyui navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+export default function Navbar() {
+  return (
+    <div className="navbar bg-[#0b0b0b] py-14 text-[#999999] opacity-90">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl normal-case">UI</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1 text-lg">
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Work</a>
+          </li>
+          <li>
+            <a>Blog</a>
+          </li>
+          <li>
+            <a>Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
