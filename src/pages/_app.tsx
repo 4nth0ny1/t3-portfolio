7db79a01ee03e60970d51dd14a2aca811d973b40
4nth0ny1 <acatullo4@gmail.com>
2023-07-15 22:57:57 -0500

    added preloader

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,42 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import Navbar from "../components/Navbar";
+import { useState, useEffect } from "react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(!loading), 1000);
+  }, []);
+
   return (
     <SessionProvider session={session}>
-      <Navbar />
-      <Component {...pageProps} />
+      {!loading ? (
+        <div className="flex h-screen w-full flex-col justify-center bg-black">
+          <h1 className="text-4xl text-[#E3E3E3]">Hello, I am Anthony.</h1>
+        </div>
+      ) : (
+        <>
+          <Navbar />
+          <Component {...pageProps} />
+        </>
+      )}
     </SessionProvider>
   );
 };
 
 export default api.withTRPC(MyApp);
+
+// sequence
+/*
+black screen text
+h1 is typed 
+Then h2s appear
+change text 
+then background fades to another color
+website components fade in
+*/
