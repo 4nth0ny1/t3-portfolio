e4525bd60dce74fe3e52f16faeebd05e4fef5416
4nth0ny1 <acatullo4@gmail.com>
2023-07-23 22:19:41 -0500

    added more pages\

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,59 +1,71 @@
 import { motion } from "framer-motion";
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <div className="navbar bg-[#0b0b0b] px-80 text-[#999999] opacity-90">
       <div className="flex-1">
-        <motion.a
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
-          className="btn btn-ghost text-xl normal-case"
-        >
-          UI
-        </motion.a>
+        <Link href="/">
+          <motion.a
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 1 }}
+            className="btn btn-ghost text-xl normal-case"
+          >
+            UI
+          </motion.a>
+        </Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1 text-lg">
-          <li>
-            <motion.a
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1 }}
-              className="btn btn-ghost text-xl normal-case"
-            >
-              About
-            </motion.a>
-          </li>
-          <li>
-            <motion.a
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1 }}
-              className="btn btn-ghost text-xl normal-case"
-            >
-              Work
-            </motion.a>
-          </li>
-          <li>
-            <motion.a
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1 }}
-              className="btn btn-ghost text-xl normal-case"
-            >
-              Blog
-            </motion.a>
-          </li>
-          <li>
-            <motion.a
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 1 }}
-              className="btn btn-ghost text-xl normal-case"
-            >
-              Contact
-            </motion.a>
-          </li>
+          <Link href="/about">
+            <li>
+              <motion.a
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+                className="btn btn-ghost text-xl normal-case"
+              >
+                About
+              </motion.a>
+            </li>
+          </Link>
+          <Link href="/work">
+            <li>
+              <motion.a
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+                className="btn btn-ghost text-xl normal-case"
+              >
+                Work
+              </motion.a>
+            </li>
+          </Link>
+          <Link href="/blog">
+            <li>
+              <motion.a
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+                className="btn btn-ghost text-xl normal-case"
+              >
+                Blog
+              </motion.a>
+            </li>
+          </Link>
+          <Link href="/contact">
+            <li>
+              <motion.a
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1 }}
+                className="btn btn-ghost text-xl normal-case"
+              >
+                Contact
+              </motion.a>
+            </li>
+          </Link>
         </ul>
       </div>
     </div>
